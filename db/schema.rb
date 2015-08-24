1d311f0fff009ec6e790d230be476a2f64c755df
Natalie Rizk <natrizk4@gmail.com>
2015-08-24 17:42:57 +0000

    more debugging

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820183231) do
+ActiveRecord::Schema.define(version: 20150824172915) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "username"
-    t.string "picture"
-    t.string "caption"
+    t.string  "picture"
+    t.string  "caption"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email_address"
+    t.string "email"
   end
 
 end
