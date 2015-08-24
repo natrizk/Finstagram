4eda188b521119d2ca734e7f627efdb4ddceaa62
marisolestrella <marisol213@icloud.com>
2015-08-24 15:17:12 +0000

    modify table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819173315) do
+ActiveRecord::Schema.define(version: 20150820183231) do
 
   create_table "posts", force: :cascade do |t|
     t.string "username"
     t.string "picture"
     t.string "caption"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email_address"
   end
 
 end
